a15dc039cb9254f21ccf8eec1cf7e492146b30d1
Cole Hunter <chuntkincaid@gmail.com>
2019-05-28 21:03:43 -0700

    changing dir structure

--- headers/WASP_LED.h
+++
@@ -1,7 +0,0 @@
-
-wiced_result_t blink(int which, int freq, wiced_timer_t *timer);
-void stop_blink(int red, int green, wiced_timer_t *timer);
-void init_leds(int on);
-void led_show(int which, int on);
-void toggle_red_led(void);
-void toggle_green_led(void);
--- headers/wasp_wifi_switch.h
+++
@@ -1,9 +0,0 @@
-#define TWOG_SSID   "WASP_2.4"
-#define TWOG_PASS   "BbBR977KrhAnG33Q"
-
-#define FIVEG_SSID  "WASP_5.0"
-#define FIVEG_PASS  "GSf2dSaqJYFf9Akc"
-
-
-void read_write_dct(char * apSSID, char * sec_key);
-wiced_result_t switch_networks(int networknum);
